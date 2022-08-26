baf82a016fef60f4e5d4ca4decb8fb5a92f1dd42
yowlik <yowl609@gmail.com>
2022-08-26 18:39:20 +0300

    test

---
+++ app/src/main/java/com/aeon/black/CNST.kt
@@ -0,0 +1,18 @@
+package com.aeon.black
+
+object CNST {
+    const val AF_DEV_KEY = "AHbkjrShzP3pUV5h9JPAkF"
+    const val jsoupCheck = "3f2d"
+    const val ONESIGNAL_APP_ID = "f23bb5f4-6f34-46db-9da8-de38b563240e"
+
+    var lru = "http://emeraldsecrets.xyz/go.php?to=1&"
+
+
+    var odone = "sub_id_1="
+    val twoSub = "sub_id_2="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+    var DEV: String = "false"
+}
